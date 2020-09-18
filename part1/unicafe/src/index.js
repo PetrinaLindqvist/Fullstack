644c998d6e39e8c5b2a0b1b9839724e6c0d7a0cc
Petrina Lindqvist <petrina.lindqvist@centria.fi>
2020-09-18 14:59:09 +0300

    buttons done 1.6

--- part1/unicafe/src/index.js
+++ part1/unicafe/src/index.js
@@ -1,17 +1,45 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React, { useState } from 'react'
+import ReactDOM from 'react-dom'
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+
+const App = (props) => {
+  // save clicks of each button to own state
+  
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+
+  const increaseByOneLeft = () => setGood(good + 1)
+  const increaseByOneMiddle = () => setNeutral(neutral + 1)
+  const increaseByOneRight = () => setBad(bad + 1)
+
+  const Button = ({ handleClick, text }) => (
+    <button onClick={handleClick}>
+      {text}
+    </button>
+  )
+  return (
+    <div>
+      
+    <Button
+      handleClick={increaseByOneLeft}
+      text='good'
+    />
+    <Button
+      handleClick={increaseByOneMiddle} 
+      text='neutral'
+    />     
+    <Button
+      handleClick={increaseByOneRight}
+      text='bad'
+    />           
+    </div>
+
+   )
+  }
+
+ReactDOM.render(<App />, 
   document.getElementById('root')
-);
+)
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+
