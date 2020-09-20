0e714414d4c984cc7f5420b7bfebe73401bdfb44
Petrina Lindqvist <petrina.lindqvist@centria.fi>
2020-09-20 17:23:20 +0300

    exercise 1.6 done

--- part1/unicafe/src/index.js
+++ part1/unicafe/src/index.js
@@ -1,26 +1,32 @@
 import React, { useState } from 'react'
 import ReactDOM from 'react-dom'
 
-
-const App = (props) => {
+const Button = ({ handleClick, text }) => {
+  return (
+  <button onClick={handleClick}>
+    {text}
+  </button>
+ )
+}
+const Statistics = (props) => {
+  return (
+    <div>{props.text} {props.value}</div>
+  )
+}
+const App = () => {
   // save clicks of each button to own state
   
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
 
-  const increaseByOneLeft = () => setGood(good + 1)
-  const increaseByOneMiddle = () => setNeutral(neutral + 1)
-  const increaseByOneRight = () => setBad(bad + 1)
+  const increaseByOneLeft = () => {setGood(good + 1)}
+  const increaseByOneMiddle = () => {setNeutral(neutral + 1)}
+  const increaseByOneRight = () => {setBad(bad + 1)}
 
-  const Button = ({ handleClick, text }) => (
-    <button onClick={handleClick}>
-      {text}
-    </button>
-  )
   return (
     <div>
-      
+      <h1>give feedback</h1>
     <Button
       handleClick={increaseByOneLeft}
       text='good'
@@ -32,12 +38,15 @@ const App = (props) => {
     <Button
       handleClick={increaseByOneRight}
       text='bad'
-    />           
+    /> 
+     <h1>statistics</h1>
+     <Statistics text='good' value={good} />
+     <Statistics text='neutral' value={neutral} />
+     <Statistics text='bad' value={bad} />
     </div>
-
    )
-  }
-
+  
+  } 
 ReactDOM.render(<App />, 
   document.getElementById('root')
 )
