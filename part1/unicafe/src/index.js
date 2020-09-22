d1fb0502ea509a459eac41d6ebbcf09cd9d78740
Petrina Lindqvist <petrina.lindqvist@centria.fi>
2020-09-22 11:30:31 +0300

    1.7 and 1.8 done

--- part1/unicafe/src/index.js
+++ part1/unicafe/src/index.js
@@ -19,10 +19,16 @@ const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
-
+ 
+ 
   const increaseByOneLeft = () => {setGood(good + 1)}
   const increaseByOneMiddle = () => {setNeutral(neutral + 1)}
   const increaseByOneRight = () => {setBad(bad + 1)}
+ 
+  const all = <all value={good + neutral + bad} />
+  const average = <average value={(good * 1 + neutral * 0 +  bad * -1)/ all} />
+  const positive = <positive value={good / all * 100 + '%'} />
+
 
   return (
     <div>
@@ -43,6 +49,9 @@ const App = () => {
      <Statistics text='good' value={good} />
      <Statistics text='neutral' value={neutral} />
      <Statistics text='bad' value={bad} />
+     <Statistics text='all' value={good + neutral + bad} />
+     <Statistics text='average' value={(good * 1 + neutral * 0 +  bad * -1) / all} />
+     <Statistics text='positive' value={(good / all * 100) + ' %'} />
     </div>
    )
   
