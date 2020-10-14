58de024460262d77370fe82c15c77ef36bce1e6d
Petrina Lindqvist <petrina.lindqvist@centria.fi>
2020-10-14 10:25:40 +0300

    index changes

--- part2/phonebook/src/index.js
+++ part2/phonebook/src/index.js
@@ -1,15 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import axios from 'axios'
 
-const promise = axios.get('http://localhost:3001/persons')
-promise.then(response => {
-  console.log(response)
-})
 
-/*const promise2 = axios.get('http://localhost:3001/foobar')
-console.log(promise2)*/
+
 
 ReactDOM.render(<App />, document.getElementById('root'))
 
