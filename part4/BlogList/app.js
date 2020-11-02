40c8fd9c786679bc310ad5c731989d90dd13cefe
Petrina Lindqvist <petrina.lindqvist@centria.fi>
2020-11-02 14:05:22 +0200

    Ex. 4.8 done

--- part4/BlogList/app.js
+++ part4/BlogList/app.js
@@ -7,14 +7,16 @@ const middleware = require('./utils/middleware')
 const logger = require('./utils/logger')
 const mongoose = require('mongoose')
 
+
 //logger.info('connecting to', config.MONGODB_URI)
 
 mongoose.connect(config.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true })
   .then(() => {
     logger.info('connected to MongoDB')
+    
   })
   .catch((error) => {
-    logger.error('error connecting to MongoDB:', error.message)
+    logger.info('error connecting to MongoDB:', error.message)
   })
 
 app.use(cors())
---
+++ part4/BlogList/tests/blog_api.test.js
@@ -0,0 +1,44 @@
+const mongoose = require('mongoose')
+const supertest = require('supertest')
+const app = require('../app')
+const api = supertest(app)
+const Blog = require('../models/blog')
+
+const initialBlogs = [
+    {
+      content: 'HTML is easy',
+      date: new Date(),
+      important: false,
+    },
+    {
+      content: 'Browser can execute only Javascript',
+      date: new Date(),
+      important: true,
+    },
+  ]
+  beforeEach(async () => {
+    await Blog.deleteMany({})
+    let blogObject = new Blog(initialBlogs[0])
+    await blogObject.save()
+    //jest.setTimeout(10000)
+    blogObject = new Blog(initialBlogs[1])
+    await blogObject.save()
+  })
+ 
+
+test('blogs are returned as json', async () => {
+  await api
+    .get('/api/blogs')
+    .expect(200)
+    .expect('Content-Type', /application\/json/)
+})
+
+test('correct amount of blogs', async () => {
+    const response = await api.get('/api/blogs')
+
+})
+
+afterAll(() => {
+  mongoose.connection.close()
+  
+})
