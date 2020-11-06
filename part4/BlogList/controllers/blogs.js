da05a4b530164f3dd5270f0c0eb702d3b93ad282
Petrina Lindqvist <petrina.lindqvist@centria.fi>
2020-11-06 13:16:07 +0200

    Ex. 4.19 done

--- part4/BlogList/controllers/blogs.js
+++ part4/BlogList/controllers/blogs.js
@@ -1,10 +1,17 @@
 const blogsRouter = require('express').Router()
-
 const Blog = require('../models/blog')
 const User = require('../models/user')
+const jwt = require('jsonwebtoken')
 
+const getTokenFrom = request => {
+  const authorization = request.get('authorization')
+  if (authorization && authorization.toLowerCase().startsWith('bearer ')) {
+    return authorization.substring(7)
+  }
+  return null
+}
 
-  blogsRouter.get('/', async (request, response) => {
+blogsRouter.get('/', async (request, response) => {
     const blogs = await Blog
       .find({}).populate('user', { username: 1, name: 1 })
   response.json(blogs.map(blog => blog.toJSON()))
@@ -25,15 +32,21 @@ blogsRouter.get('/:id', (request, response, next) => {
 
 blogsRouter.post('/', async (request, response) => {
   const body = request.body
+  const token = getTokenFrom(request)
+  const decodedToken = jwt.verify(token, process.env.SECRET)
+  if (!token || !decodedToken.id) {
+    return response.status(401).json({ error: 'token missing or invalid' })
+  }
+  const user = await User.findById(decodedToken.id)
   //const users = await User.find({})
-  const user = await User.findById(users[0]._id)
+  //const user = await User.findById(users[0]._id)
 
   const blog = new Blog({
     title: body.title,
     author: body.author,
     url: body.url,
     likes: body.likes || 0,
-    user: users[0]._id
+    user: user._id
   })
   
   try {
